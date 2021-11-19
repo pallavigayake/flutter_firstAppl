6cad95b8f16229b433b50268bd63349e1f16d064
Pallavi.Gayake <pgayake22@gmail.com>
2021-11-19 11:42:01 +0530

    this app is about widget

---
+++ android/app/src/main/kotlin/com/example/youtube_tutorial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.youtube_tutorial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MaterialApp(
+    title: "awesome Project",
+    home: HomePage(),
+  ));
+}
+class HomePage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Awesome App"),
+      ),
+    body: Container(
+      child: Center(child: Text("Hi Flutter")),
+    ),
+    );
+
+  }
+}
+
+
